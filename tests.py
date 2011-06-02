c86e691061bd8744a240d03aaf6e84cbf5f2eca8
Likit Preeyanon <preeyano@msu.edu>
2011-06-01 22:22:47 -0400

    deleted some print statements

--- tests.py
+++ tests.py
@@ -1,7 +1,6 @@
 import exon_grouper as eg
 import sys
 def testClusters1():
-    print >> sys.stderr, 'Test 1 -----------------'
     exons = {}
     tStartsSets = [[100, 300, 500, 700],
                     [150, 300,500, 700,800],
@@ -22,7 +21,6 @@ def testClusters1():
     assert starts == ['0', '200', '400','600', '700']
 
 def testClusters2():
-    print >> sys.stderr, 'Test 2 -----------------'
     exons = {}
     tStartsSets = [[100, 300, 500, 700],
                     #[150, 300,500, 700,800],
@@ -51,7 +49,6 @@ def testClusters2():
     assert starts == ['0', '100', '250', '450', '650']
 
 def testClusters3():
-    print >> sys.stderr, 'Test 3 ----------------'
     exons = {}
     tStartsSets = [#[100, 300, 500, 700],
                     #[150, 300,500, 700,800],
@@ -80,7 +77,6 @@ def testClusters3():
     assert starts == ['0', '100', '250', '450', '650']
 
 def testClusters4():
-    print >> sys.stderr, 'Test 4 -----------------'
     exons = {}
     tStartsSets = [#[100, 300, 500, 700],
                     #[150, 300,500, 700,800],
@@ -107,7 +103,6 @@ def testClusters4():
     assert starts == ['0', '55', '255', '455', '655', '755']
 
 def testClusters5():
-    print >> sys.stderr, 'Test 5 -----------------'
     exons = {}
     tStartsSets = [[100, 300, 500, 700],
                     #[150, 300,500, 700,800],
@@ -130,7 +125,5 @@ def testClusters5():
     starts, sizes = eg.printBed(geneModels)
     print >> sys.stderr, 'sizes = ', sizes
     print >> sys.stderr, 'starts = ', starts
-    for e in exons.itervalues():
-        print >> sys.stderr, e.start, e.end, e.junctions
     assert sizes == ['150', '50', '120', '200']
     assert starts == ['0', '250', '450', '650']
