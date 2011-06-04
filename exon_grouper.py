7a78adb288212191527e958f3579f93c35ee6e9e
Likit Preeyanon <preeyano@msu.edu>
2011-06-04 15:58:04 -0400

    added getSequenceExonWise()

--- exon_grouper.py
+++ exon_grouper.py
@@ -19,6 +19,7 @@
 import psl_parser
 import sys
 import csv
+#from pygr import seqdb, sequtil
 
 class Exon(object):
 
@@ -271,6 +272,19 @@ def buildGeneModels(exons, exonClusters, clusterReferences):
 
     return geneModels
 
+def getSequenceGeneWise(geneModels, genome):
+    for ref in geneModels:
+        transcriptNumber = 1
+        exonNumber = 1
+        op = open(ref+'.fasta', 'w')
+        for exon in geneModels[ref]:
+            start, end = exon
+            seq = genome[ref][start:end]
+            exonID = 'exon_%d' % exonNumber
+            sequtil.write_fasta(op, id=exonID)
+            exonNumber += 1
+        op.close()
+
 def printBed(geneModels):
 
     writer = csv.writer(sys.stdout, dialect='excel-tab')
@@ -328,4 +342,6 @@ if __name__ == '__main__':
     exonClusters, clusterReferences = cluster(exons)
     print >> sys.stderr, 'Building gene models ...'
     geneModels = buildGeneModels(exons, exonClusters, clusterReferences)
+    #genome = seqdb.SequenceFileDB(sys.argv[2])
+    #getSequenceExonWise(geneModels)
     sizes, starts = printBed(geneModels)
