dcaae46da12d893b0d16afd3fd9e2e94955f74cf
Likit Preeyanon <preeyano@msu.edu>
2011-07-08 23:37:28 -0400

    get it works for doing BLAST from the internet and parsing BLAST output

--- geneModelVal.py
+++ geneModelVal.py
@@ -1,73 +1,32 @@
 import sys
-import csv
 from pygr import seqdb
-from Bio.Seq import Seq
-from Bio.Alphabet import IUPAC
+from Bio import SeqIO
+from Bio.Blast.Applications import NcbiblastpCommandline
+from Bio.Blast import NCBIWWW, NCBIXML
 
-class Isoform(object):
-    """docstring for Isoform"""
-    def __init__(self, genome, *args):
-        self.chrom = args[0]
-        self.chromStart = int(args[1])
-        self.chromEnd = int(args[2])
-        self.name = args[3]
-        self.score = args[4]
-        self.strand = args[5]
-        self.startCodon = int(args[6])
-        self.stopCodon = int(args[7])
-        self.itemRgb = args[8]
-        self.blockCount = args[9]
-        self.blockSizes = [int(s) for s in args[10].split(',')]
-        self.blockStarts = [int(s) for s in args[11].split(',')]
-        self.exons = self.getExons()
-        self.dnaSeq = self.getDnaSequence()
-        self.proteinSeq = self.getProteinSequence()
+for seqRecord in SeqIO.parse('proteins.fa', 'fasta'):
+    print 'doing BLAST search for', seqRecord.id
+    ratios = []
+    resultHandle = NCBIWWW.qblast('blastp', 'nr', seqRecord.seq)
+    blastRecords = NCBIXML.parse(resultHandle)
+    for blastRecord in blastRecords:
+        for alignment in blastRecord.alignments:
+            for hsp in alignment.hsps:
+                if hsp.expect < 0.04 and 'Mus' in alignment.title: 
+                    #print '----Alignment----'
+                    print 'sequence:', alignment.title
+                    #print 'length:', alignment.length
+                    #print 'bits:', hsp.bits
+                    #print 'identity:', hsp.identities
+                    print 'bits/length:', hsp.bits/alignment.length
+                    print 'identity/length:', hsp.identities/float(alignment.length)
+                    ratios.append(hsp.bits/alignment.length)
+                    '''
+                    print hsp.query[0:75] + '...'
+                    print hsp.match[0:75] + '...'
+                    print hsp.sbjct[0:75] + '...'
+                    '''
+    print sorted(ratios)
 
-    def getDnaSequence(self):
-        seq = ''
-        for start, stop in self.exons:
-            seq += str(genome[self.chrom][start:stop])
-        bioSeq = Seq(seq.upper(), IUPAC.unambiguous_dna)
-        if self.strand == '+':
-            return bioSeq 
-        else:
-            return bioSeq.reverse_complement()
-
-    def getProteinSequence(self):
-        orfStart = self.startCodon-self.chromStart
-        orfEnd = self.stopCodon-self.chromStart
-        self.orf = self.dnaSeq[orfStart:orfEnd]
-        print 'ORF start, end = ', orfStart, orfEnd
-        return self.orf.translate()
-
-    def getExons(self):
-        exons = []
-        for i in range(len(self.blockStarts)):
-            start = self.blockStarts[i]
-            stop = self.blockSizes[i] + self.blockStarts[i]
-            exons.append((start,stop))
-        return exons
-        
-allGenes = {}
-
-genome = seqdb.SequenceFileDB(sys.argv[2])
-modelReader = csv.reader(open(sys.argv[1], 'rb'), delimiter='\t')
-for row in modelReader:
-    name = row[3]
-    chrom = row[0]
-    chrom, geneID, isoID, _, frame = name.split('_')
-    isoform = Isoform(genome, *row)
-    if chrom not in allGenes:
-        allGenes[chrom] = {}
-        allGenes[chrom][geneID] = [isoform]
-    else:
-        try:
-            allGenes[chrom][geneID].append(isoform)
-        except KeyError:
-            allGenes[chrom][geneID] = [isoform]
-
-print 'total gene:', len(allGenes)
-print isoform.name, isoform.chromStart, isoform.chromEnd, isoform.chrom, isoform.startCodon, isoform.stopCodon
-print isoform.exons, isoform.strand
-print isoform.orf
-print isoform.proteinSeq
+#def validateORF(seqRecord):
+#blastpCommand = NcbiblastpCommandline(query='proteins.fa', db='nr', out='proteinSample.xml')
