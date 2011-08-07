8e87d51b7e3de843076fc8588b4fa6ee80a58680
Likit Preeyanon <preeyano@msu.edu>
2011-08-07 17:20:40 -0400

    fixed an error of junction.coverage in all tests in TestFindMatch class

--- test_junction_comp.py
+++ test_junction_comp.py
@@ -64,88 +64,122 @@ class TestGetJunction(TestCase):
         junctions = [junc for junc in jc.getJunction(self.row)]
         self.assertEqual(len(junctions), 1)
 
+
 class TestFindMatch(TestCase):
 
     def test_find_match_matched(self):
         key = 'chr1:154000-230000'
         mocker = Mocker()
+
         junction = mocker.mock()
+        junction.coverage
+        mocker.result(40)
+        mocker.count(1, None)
+
         container = mocker.mock()
         container.keys()
         mocker.result([key])
+
         container[key]
         mocker.result(junction)
+        mocker.count(1, None)
+
         mocker.replay()
+
         self.common, self.diff = jc.findMatch(container, container)
         self.assertEqual(self.common.keys(), ['chr1:154000-230000'])
         self.assertEqual(self.diff.keys(), [])
 
+        mocker.restore()
+        mocker.verify()
+
     def test_find_match_not_matched(self):
         key1 = 'chr1:154000-230000'
-        key2 = 'chr1:155000-230000'
         mocker = Mocker()
+
         junction = mocker.mock()
+        junction.coverage
+        mocker.result(40)
+
         container1 = mocker.mock()
         container2 = mocker.mock()
 
         container1.keys()
         mocker.result([key1])
-        container2.keys()
-        mocker.result([key2])
 
         container1[key1]
         mocker.result(junction)
+
         container2[key1]
         mocker.throw(KeyError)
+        mocker.count(1)
 
         mocker.replay()
+
         self.common, self.diff = jc.findMatch(container1, container2)
         self.assertEqual(self.common.keys(), [])
         self.assertEqual(self.diff.keys(), [key1])
+
+        mocker.restore()
+        mocker.verify()
 
     def test_find_match_match_not_match(self):
         key1 = ['chr1:154000-230000', 'chr1:155000-230000']
         key2 = ['chr1:154000-230000']
         mocker = Mocker()
         junction = mocker.mock()
+        junction.coverage
+        mocker.result(40)
+        mocker.count(1, None)
+
         container1 = mocker.mock()
         container2 = mocker.mock()
 
         container1.keys()
         mocker.result(key1)
-        container2.keys()
-        mocker.result(key2)
 
         container1[key1[0]]
         mocker.result(junction)
+        mocker.count(1, None)
+
         container1[key1[1]]
         mocker.result(junction)
+        mocker.count(1, None)
+
         container2[key1[0]]
         mocker.result(junction)
+        mocker.count(1, None)
+
         container2[key1[1]]
         mocker.throw(KeyError)
+        mocker.count(1)
 
         mocker.replay()
         self.common, self.diff = jc.findMatch(container1, container2)
         self.assertEqual(self.common.keys(), [key1[0]])
         self.assertEqual(self.diff.keys(), [key1[1]])
 
+        mocker.restore()
+        mocker.verify()
+
+
 class TestJunctionScan(TestCase):
-    def setUp(self):
+
+    def test_shared_exon(self):
         mocker = Mocker()
         self.model = mocker.mock()
         self.model.blockStarts
         mocker.result([100, 400, 700])
         self.model.blockEnds
         mocker.result([200, 500, 800])
-        self.chromName
+        self.model.chromName
         mocker.result('chr1')
         mocker.replay()
-        self.junctions1 = {200:400, 500:700}
-        self.junctions2 = {200:700}
+        self.junctions1 = {200: 400, 500: 700}
+        self.junctions2 = {200: 700}
+        self.sharedExons = jc.scanJunctions(self.model,
+                                self.junctions1, self.junctions2)
 
-    def test_shared_exon(self):
-        self.sharedExons = jc.scanJunctions(self.model, self.junctions1, self.junctions2)
         for j in self.sharedExons:
             self.assertEqual(j, [400, 720])
 
